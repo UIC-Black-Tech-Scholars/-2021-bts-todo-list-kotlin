5ac4dca612f89e79eb61e89ed94ae5f7b8024a0f
lucasoskorep <lucas.oskorep@gmail.com>
2020-11-09 16:41:09 -0600

    Adding list and instruction comments to the code

--- app/src/main/java/com/example/bts_todo_list_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/bts_todo_list_kotlin/MainActivity.kt
@@ -7,11 +7,17 @@ import android.widget.ListView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var listView: ListView
+    private lateinit var dataList: List<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //find list view
+
+        //create list data
+
+        //hook list data up to our list using array adapter
 
     }
 }
