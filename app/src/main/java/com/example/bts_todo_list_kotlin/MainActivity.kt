a40e24205a5d5620c4c16940c71e7c0dda312a19
lucasoskorep <lucas.oskorep@gmail.com>
2020-11-09 16:35:46 -0600

    Adding in base project

---
+++ app/src/main/java/com/example/bts_todo_list_kotlin/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.bts_todo_list_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
