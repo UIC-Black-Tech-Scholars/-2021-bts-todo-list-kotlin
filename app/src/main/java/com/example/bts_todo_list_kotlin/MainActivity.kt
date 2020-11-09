45d55d50eafe48acecd3893168285bcd8feb58c6
lucasoskorep <lucas.oskorep@gmail.com>
2020-11-09 17:06:07 -0600

    Adding in code for creating the list View

--- app/src/main/java/com/example/bts_todo_list_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/bts_todo_list_kotlin/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.bts_todo_list_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
-
+    private var TAG = "MainActivity"
     private lateinit var listView: ListView
     private lateinit var dataList: List<String>
 
@@ -14,10 +17,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //find list view
-
+        listView = findViewById(R.id.list_view)
         //create list data
-
+        dataList = listOf("This", "Is", "A", "List", "Of", "Strings", "This", "Is", "A", "List", "Of", "Strings", "This", "Is", "A", "List", "Of", "Strings", "This", "Is", "A", "List", "Of", "Strings", "This", "Is", "A", "List", "Of", "Strings")
+        dataList.forEach{
+                item -> Log.d(TAG, item)
+        }
         //hook list data up to our list using array adapter
-
+        var listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dataList.toTypedArray())
+        listView.adapter = listAdapter
     }
 }
